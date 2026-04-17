1fc97a0a62091fd1b5592ccf8deff36ec898031c
Raanan Weber <raananw+github@gmail.com>
2026-04-17 10:17:28 +0200

    Enable ESLint naming convention rule for constantBlockPropertyComponent

--- packages/tools/flowGraphEditor/src/graphSystem/properties/constantBlockPropertyComponent.tsx
+++ packages/tools/flowGraphEditor/src/graphSystem/properties/constantBlockPropertyComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as React from "react";
 import { LineContainerComponent } from "../../sharedComponents/lineContainerComponent";
 import { type IPropertyComponentProps } from "shared-ui-components/nodeGraphSystem/interfaces/propertyComponentProps";
